ee033ba1cada7fe493ba294155a98979740cc5f1
moskalevgeorge83-sys <moskalevgeorge83@gmail.com>
2026-02-09 18:11:29 +0500

    Update settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -162,3 +162,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = reverse_lazy("myauth:about-me")
 LOGIN_URL = reverse_lazy("myauth:login")
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
+
